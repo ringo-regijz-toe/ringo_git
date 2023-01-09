ce7dfd9b1ee397ddfc7ddbd00fce7212a9c570db
ringo-regijz-toe <122245337+ringo-regijz-toe@users.noreply.github.com>
2023-01-09 14:43:29 +0900

    first commit
    practice

---
+++ projectA/index.js
@@ -0,0 +1,21 @@
+let youtuber = {
+  list: {
+     business: {
+         youtuber: [
+           { name:"りんご",age: 16,teachmusicgame() {} },
+           { name:"だざん",age: 999999,playgame() {} }       
+        ],
+        teach() {}
+     },
+     entertainment: {
+        youtuber: [{ name:"ヒカキン"},{name:"はじめん"}],
+        makesmile() {}
+     }
+  },
+  Plan() {},
+  uploadVideo() {}
+};
+
+
+youtuber.list.business.youtuber[0].teachmusicgame();
+
